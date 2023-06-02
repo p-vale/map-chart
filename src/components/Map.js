5c0d162f5733141fc5002fdfd15347cd57bf54f9
Valentina Pezzi <valente.pezzi@gmail.com>
2023-06-02 18:23:59 +0200

    basemap

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,16 @@
-import styled from "styled-components"
-
-const Wrapper = styled.div`
-  height: 100vh;
-  flex-grow: 1;
-  padding: 20px;
-  background-color: blue;
-`
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css"
+import "./map/map.css"
 
 const Map = () => {
   return(
-    <Wrapper></Wrapper>
+    <MapContainer center={[50.5, 30.5]} zoom={13}>
+      <TileLayer
+        attribution=""
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={19}
+      />
+    </MapContainer>
   )
 }
 
