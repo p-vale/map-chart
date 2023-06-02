e5b504ecfefa37649569bfd99a4bd7e7922b1602
Valentina Pezzi <valente.pezzi@gmail.com>
2023-06-02 21:08:32 +0200

    data

---
+++ src/taxonomy.js
@@ -0,0 +1,169 @@
+export const taxonomy = {
+  5: {
+    name: "Mobilità e Trasporto",
+    class: {
+      503: {
+        name: "Rete dei trasporti",
+        type: {
+          50301: "Stazione Ferroviaria",
+        },
+      },
+    },
+  },
+  7: {
+    name: "Istruzione",
+    class: {
+      701: {
+        name: "Altri Istituti",
+        type: {
+          70102: "Enti di Formazione",
+          70101: "Altri Istituti",
+        }
+      },
+      702: {
+        name: "Infanzia",
+        type: {
+          70207: "Scuole d'infanzia",
+          70204: "Nidi d'infanzia Sez. Primavera",
+          70206: "Nido d'infanzia Aggregato",
+          70201: "Nidi d'infanzia",
+          70203: "Nidi d'infanzia Serv. Sperimentali",
+          70202: "Nidi d'infanzia Serv. Domiciliari",
+        }
+      },
+      703: {
+        name: "Scolastico",
+        type: {
+          70301: "Scuola Secondaria di Secondo Grado",
+          70302: "Scuole Primaria",
+          70303: "Scuole Secondarie di Primo Grado",
+        }
+      },
+      704: {
+        name: "Università",
+        type: {
+          70401: "Università",
+        }
+      },
+    },
+  },
+  8: {
+    name: "Commercio",
+    class: {
+      803: {
+        name: "Commercio ambulante",
+        type: {
+          80301: "Mercati",
+        },
+      },
+    },
+  },
+  10: {
+    name: "Ambiente e Natura",
+    class: {
+      1002: {
+        name: "Animali",
+        type: {
+          100201: "Area Cani",
+        },
+      },
+    },
+  },
+  1: {
+    name: "Cultura",
+    class: {
+      102: {
+        name: "Centri della Cultura",
+        type: {
+          10205: "Fiere e Centri Congressi",
+          10207: "Teatri e Cinema",
+          10204: "Biblioteche",
+        },
+      }
+    },
+  },
+  2: {
+    name: "Pubblica ammistrazione",
+    class: {
+      201: {
+        name: "Enti Nazionali",
+        type: {
+          20202: "Istituti Agenzie Regionali",
+        },
+      },
+      202: {
+        name: "Enti Regionali",
+        type: {
+          20101: "Sedi Organi Centrali",
+          20102: "Istituti Agenzie Nazionali",
+        },
+      },
+      203: {
+        name: "Enti Locali",
+        type: {
+          20304: "Sedi Aree Territoriali",
+          20303: "Sedi Comunali",
+        },
+      },
+    },
+  },
+  12: {
+    name: "Ricreativo",
+    class: {
+      1202: {
+        name: "Impianti sportivi",
+        type: {
+          120218: "Polisportiva",
+          120205: "Campo da calcio",
+          120207: "Campo da tennis",
+          120210: "Centro addestramento cani",
+          120215: "Piastra polivalente",
+          120214: "Palestra",
+          120216: "Piscina",
+          120220: "Stadio",
+          120202: "Bocciodromo",
+          120217: "Pista d'atletica",
+          120213: "Palasport",
+          120212: "Invaso sport acquatici",
+          120201: "Aviosuperfice",
+          120208: "Centro ippico",
+          120206: "Campo da rugby",
+          120211: "Campo da football americano",
+          120221: "Tiro a segno",
+          120204: "Campo da calcetto",
+          120209: "Circuito",
+          120203: "Campo da baseball",
+          120219: "Skatepark",
+        },
+      },
+    },
+  },
+  3: {
+    name: "Sociale",
+    class: {
+      301: {
+        name: "Associazioni di Volontariato",
+        type: {},
+      },
+      303: {
+        name: "Centri sociali e di aggregazione",
+        type: {
+          30102: "Associazioni di Volontariato",
+          30302: "Centro Sociale",
+          30301: "Centro di aggregazione giovanile",
+        },
+      }, 
+    },
+  },
+  13: {
+    name: "Servizi",
+    class: {
+      1303: {
+        name: "Servizi pubblici",
+        type: {
+          130301: "Poste Italiane",
+        },
+      },
+    },
+  }
+}
